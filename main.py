10abc5c5908bbfdd363e73e20528d94f00c35828
a-salamandra <la.februaro@gmail.com>
2022-04-11 15:09:08 +0300

    v1.1

--- main.py
+++ main.py
@@ -1,63 +1,56 @@
 def display_board():
-
-    print (f"""  Ход № {turn_count}!
+    print(f"""  Ход № {len(x_moves + o_moves) + 1}!
     {board[1]} | {board[2]} | {board[3]}
     ---------
     {board[4]} | {board[5]} | {board[6]}
     ---------
     {board[7]} | {board[8]} | {board[9]}""")
 
-def make_turn(turn):
 
-    player = "O" if turn%2 == 0 else "X"
+def make_turn():
+    global is_it_x_turn
+    player_mark = "X" if is_it_x_turn else "O"
 
-    space = int(input(f"Игрок {player} укажите клетку: "))
-
+    space = int(input(f"Игрок {player_mark} укажите клетку: "))
     while space not in legal_moves:
-        space = int(input(f"Игрок {player} укажите правильную клетку: "))
+        space = int(input(f"Игрок {player_mark} укажите правильную клетку: "))
 
-    board[space] = player
+    board[space] = player_mark
     legal_moves.remove(space)
 
-def is_there_a_winner():
+    if player_mark == "X":
+        x_moves.append(space)
+    else:
+        o_moves.append(space)
+    is_it_x_turn = not is_it_x_turn
 
-    if board[1] == board[2] == board[3]:
-        print(f"Игрок {board[1]} победил!")
-        return True
-    elif board[4] == board[5] == board[6]:
-        print(f"Игрок {board[4]} победил!")
-        return True
-    elif board[7] == board[8] == board[9]:
-        print(f"Игрок {board[7]} победил!")
-        return True
-    elif board[1] == board[5] == board[9]:
-        print(f"Игрок {board[1]} победил!")
-        return True
-    elif board[3] == board[5] == board[7]:
-        print(f"Игрок {board[3]} победил!")
-        return True
-    elif board[1] == board[4] == board[7]:
-        print(f"Игрок {board[1]} победил!")
-        return True
-    elif board[2] == board[5] == board[8]:
-        print(f"Игрок {board[2]} победил!")
-        return True
-    elif board[3] == board[6] == board[9]:
-        print(f"Игрок {board[3]} победил!")
-        return True
-    elif legal_moves == []:
+
+def is_there_a_winner():
+    for win in winning_combinations:
+        if win.issubset(x_moves):
+            print(f"Игрок X победил!")
+            return True
+        elif win.issubset(o_moves):
+            print(f"Игрок O победил!")
+            return True
+
+    if legal_moves == []:
         print("Ничья")
         return True
+
     else:
         return False
+
 
 if __name__ == '__main__':
 
     board = {1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9"}
     legal_moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    turn_count = 1
+    x_moves, o_moves = [], []
+    winning_combinations = [{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7},
+                            {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}]
+    is_it_x_turn = True
 
     while not is_there_a_winner():
         display_board()
-        make_turn(turn_count)
-        turn_count += 1
+        make_turn()
